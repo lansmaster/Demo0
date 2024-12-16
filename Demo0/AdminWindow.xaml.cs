4a257c8f901ad3f1a7f1fe69dc24bc10b4165266
lansmaster <stepanbaryhev@gmail.com>
2024-12-16 23:37:03 +0300

    AdminWindow UI

---
+++ Demo0/AdminWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Demo0
+{
+    /// <summary>
+    /// Логика взаимодействия для AdminWindow.xaml
+    /// </summary>
+    public partial class AdminWindow : Window
+    {
+        public AdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonUnlockUser_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonSaveChanges_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
